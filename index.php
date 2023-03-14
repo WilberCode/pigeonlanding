703afdabe78e128fa8879d1a7d904072ad2bdeb0
wilber <parionahuaraca@gmail.com>
2023-03-13 20:19:56 -0500

    cambios

--- index.php
+++ index.php
@@ -138,7 +138,7 @@
          <div  class="relative video-container video-outline-1" >
 
             <img  class="mx-auto" src="build/img/latch-on.jpg?v=<?=theVersion();?>" alt="">
-            <video muted="false"   loop type="video/mp4" data-src="build/video/latch-on.mp4" poster="build/img/latch-on.jpg?v=<?=theVersion();?>" class="absolute lazy video lozad   bottom-0 top-0 left-0 right-0   w-full  "  >    </video>
+            <video muted="false"   loop type="video/mp4" data-src="build/video/latch-on-v2.mp4" poster="build/img/latch-on.jpg?v=<?=theVersion();?>" class="absolute lazy video lozad   bottom-0 top-0 left-0 right-0   w-full  "  >    </video>
          </div> 
        </section>
        <section  class="pt-[40px] pb-[43px]" >
@@ -194,7 +194,7 @@
             <div  class="relative video-container" >
 
                <img  class="mx-auto" src="build/img/biberon-pigeon.jpg?v=<?=theVersion();?>" alt="">
-               <video  muted data-src="build/video/biberon-pigeon.mp4" loop type="video/mp4" poster="build/img/biberon-pigeon.jpg?v=<?=theVersion();?>" class="absolute lazy video lozad bottom-0 top-0 left-0 right-0 w-full"></video>
+               <video  muted data-src="build/video/biberon-pigeon-v2.mp4" loop type="video/mp4" poster="build/img/biberon-pigeon.jpg?v=<?=theVersion();?>" class="absolute lazy video lozad bottom-0 top-0 left-0 right-0 w-full"></video>
             </div> 
          </section>
 
